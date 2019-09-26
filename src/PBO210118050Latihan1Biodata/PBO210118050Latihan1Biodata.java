1ebc0c2d63c93b44186f14cf713a99e9cf39e9b3
M-Rizky-permana <55745444+M-Rizky-permana@users.noreply.github.com>
2019-09-26 19:34:01 +0700

    Add files via upload

---
+++ src/PBO210118050Latihan1Biodata/PBO210118050Latihan1Biodata.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO210118050Latihan1Biodata ;
+
+/**
+ *
+ * @author 
+ * NAMA     : Muhamad Rizky Permana
+ * KELAS    : IF-2
+ * NIM      : 10118050
+ * Deskripsi Program : Menampilkan Value ke layar
+ */
+public class PBO210118050Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA DIRI" + "\n" + "---------------");
+        System.out.println("NAMA\t\t: Muhamad Rizky Permana");
+        System.out.println("ALAMAT\t\t: Taman Raya Cilegon");
+        System.out.println("TEMPAT LAHIR\t: Cilegon");
+        System.out.println("TANGGAL LAHIR\t: 16 Mei 2019");
+        System.out.println("---------------");
+        System.out.println("HOBI\t\t: Main Futsal");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Cendol");
+        
+        
+    }
+    
+}
